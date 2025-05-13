e759b0a3d0db60a83cf766e8cb5879122be345b9
JuhwanLee99 <jimmy3470second@gmail.com>
2025-05-13 21:06:46 +0900

    refactor: pdf 렌더링을 위젯으로 처리하기 위함

---
+++ lib/features/pdf_viewer/widgets/pdf_page_viewer.dart
@@ -0,0 +1,34 @@
+//pdf_page_viewer.dart
+
+import 'package:flutter/material.dart';
+import 'package:pdfrx/pdfrx.dart';
+
+/// PDF 문서의 모든 페이지를 스크롤 가능한 뷰로 렌더링하는 위젯
+class PdfPageViewer extends StatelessWidget {
+  final PdfDocument document;
+
+  const PdfPageViewer({super.key, required this.document});
+
+  @override
+  Widget build(BuildContext context) {
+    final pageCount = document.pages.length;
+
+    return ListView.builder(
+      itemCount: pageCount,
+      itemBuilder: (context, index) {
+        final page = document.pages[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: AspectRatio(
+            aspectRatio: page.width / page.height,
+            child: PdfPageView(
+              document: document,
+              pageNumber: index,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+

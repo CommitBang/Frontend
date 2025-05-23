46289b2ba6e6a57e86982f25cc2bcf07d3020c4c
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2025-05-23 12:04:16 +0900

    feat: Pretendard 폰트 추가 및 테마 설정 - pubspec.yaml에 Pretendard 폰트 추가 - lightTheme 및 darkTheme 정의 - 텍스트 스타일을 위한 appTextTheme 추가 - 테마 관련 파일 구조화

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
+import '/core/theme/color_scheme.dart';
 
+final ThemeData lightTheme = ThemeData(
+  colorScheme: const ColorScheme(
+    brightness: Brightness.light,
+    primary: AppColors.lightPrimary,
+    onPrimary: AppColors.lightOnPrimary,
+    primaryContainer: AppColors.lightPrimaryContainer,
+    onPrimaryContainer: AppColors.lightOnPrimaryContainer,
+    secondary: AppColors.lightSecondary,
+    onSecondary: AppColors.lightOnSecondary,
+    secondaryContainer: AppColors.lightSecondaryContainer,
+    onSecondaryContainer: AppColors.lightOnSecondaryContainer,
+    error: AppColors.lightError,
+    onError: AppColors.lightOnError,
+    errorContainer: AppColors.lightErrorContainer,
+    onErrorContainer: AppColors.lightOnErrorContainer,
+    surface: AppColors.lightSurface,
+    onSurface: AppColors.lightOnSurface,
+    surfaceContainerHighest: AppColors.lightOnSurfaceVarient,
+    outline: AppColors.lightOnSurfaceContainerVarient,
+  ),
+);
+
+final ThemeData darkTheme = ThemeData(
+  colorScheme: const ColorScheme(
+    brightness: Brightness.dark,
+    primary: AppColors.darkPrimary,
+    onPrimary: AppColors.darkOnPrimary,
+    primaryContainer: AppColors.darkPrimaryContainer,
+    onPrimaryContainer: AppColors.darkOnPrimaryContainer,
+    secondary: AppColors.darkSecondary,
+    onSecondary: AppColors.darkOnSecondary,
+    secondaryContainer: AppColors.darkSecondaryContainer,
+    onSecondaryContainer: AppColors.darkOnSecondaryContainer,
+    error: AppColors.darkError,
+    onError: AppColors.darkOnError,
+    errorContainer: AppColors.darkErrorContainer,
+    onErrorContainer: AppColors.darkOnErrorContainer,
+    surface: AppColors.darkSurface,
+    onSurface: AppColors.darkOnSurface,
+    surfaceContainerHighest: AppColors.darkOnSurfaceVarient,
+    outline: AppColors.darkOnSurfaceContainerVarient,
+  ),
+);

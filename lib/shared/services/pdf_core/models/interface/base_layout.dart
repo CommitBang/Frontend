718f4a7c9dd0e755abf2479ddf1b473b5a850e7c
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2025-05-24 11:50:56 +0900

    feat: pdf_core에 모델 추가
    - pubspec.yaml에 isar, isar_flutter_libs, path_provider 추가
    - pubspec.lock에 새로운 패키지 버전 및 의존성 추가
    - BasePDF, BasePage, BaseLayout 인터페이스 추가

---
+++ lib/shared/services/pdf_core/models/interface/base_layout.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/services.dart';
+
+enum LayoutType { formula, text, number, header, algorithm }
+
+// PDF 레이아웃 데이터 모델 인터페이스
+abstract class BaseLayout {
+  // 레이아웃 타입
+  LayoutType get type;
+
+  // 레이아웃 내용
+  String get content;
+
+  // 텍스트 레이아웃일 경우 값을 가집니다.
+  String? get text;
+
+  // 수식 레이아웃일 경우 값을 가집니다. 라텍스 형식으로 저장합니다.
+  String? get latex;
+
+  // 레이아웃 위치
+  Rect get rect;
+}
---
+++ lib/shared/services/pdf_core/models/interface/base_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/services.dart';
+import 'base_layout.dart';
+
+// PDF 페이지 데이터 모델 인터페이스
+abstract class BasePage {
+  // 페이지 번호
+  int get pageIndex;
+
+  // 페이지 크기
+  Size get size;
+
+  // 페이지 전체 텍스트
+  String get fullText;
+
+  // 레이아웃 목록
+  List<BaseLayout> getLayouts();
+}

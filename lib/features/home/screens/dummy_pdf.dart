ee4d4712ebf34399b71ebe3cc2fbff37c03d1c10
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2025-05-25 23:32:12 +0900

    fix: DummyPdf 클래스 수정 - DummyPdf 클래스에 id 게터 및 getThumbnail 메서드 추가 - getPages 메서드를 비동기로 변경

--- lib/features/home/screens/dummy_pdf.dart
+++ lib/features/home/screens/dummy_pdf.dart
@@ -1,10 +1,15 @@
+import 'dart:typed_data';
+
+import 'package:isar/isar.dart';
 import 'package:snapfig/shared/services/pdf_core/models/models.dart';
 
 // 더미 PDF 데이터용 클래스 (BasePdf 구현)
 class DummyPdf implements BasePdf {
-  final int index;
   DummyPdf({required this.index});
 
+  @override
+  Id get id => 0;
+  final int index;
   @override
   String get name => '샘플 PDF $index';
   @override
@@ -21,5 +26,11 @@ class DummyPdf implements BasePdf {
   PDFStatus get status =>
       index == 2 ? PDFStatus.processing : PDFStatus.completed;
   @override
-  List<BasePage> getPages() => [];
+  Future<List<BasePage>> getPages() async => [];
+
+  @override
+  Future<Uint8List?> getThumbnail() {
+    // TODO: implement getThumbnail
+    throw UnimplementedError();
+  }
 }

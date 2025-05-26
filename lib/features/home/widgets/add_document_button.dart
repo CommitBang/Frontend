d3f27ec16fc93c4c22b6d8b540e3cf4f92ef4ccd
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2025-05-26 12:29:01 +0900

    feat: 파일 선택 기능 추가 - AddDocumentButton 위젯에 파일 선택 기능 구현 - file_picker 패키지 추가 및 PDF 파일 선택 로직 구현 - 선택된 파일 경로를 PDFProvider에 전달하여 PDF 추가 기능 포함 - 오류 발생 시 사용자에게 Snackbar로 알림 제공

--- lib/features/home/widgets/add_document_button.dart
+++ lib/features/home/widgets/add_document_button.dart
@@ -1,13 +1,62 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:snapfig/shared/services/pdf_core/pdf_core.dart';
 
 class AddDocumentButton extends StatelessWidget {
   const AddDocumentButton({super.key});
+
+  void _pickFile(BuildContext context) async {
+    final pdfProvider = InheritedPDFProviderWidget.of(context).provider;
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowMultiple: false,
+      allowedExtensions: ['pdf'],
+    );
+    if (result != null && result.files.isNotEmpty) {
+      final file = result.files.first;
+      final path = file.path;
+      if (path != null) {
+        try {
+          await pdfProvider.addPDF(path);
+        } catch (e) {
+          if (context.mounted) {
+            _showErrorSnackbar(context, 'PDF를 불러오는데 실패했습니다.');
+          }
+        }
+      } else {
+        if (context.mounted) {
+          _showErrorSnackbar(context, '파일 경로를 가져올 수 없습니다.');
+        }
+      }
+    } else {
+      if (context.mounted) {
+        _showErrorSnackbar(context, '파일을 선택할 수 없습니다.');
+      }
+    }
+  }
+
+  void _showErrorSnackbar(BuildContext context, String message) {
+    final theme = Theme.of(context);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: theme.textTheme.bodyMedium?.copyWith(
+            color: theme.colorScheme.onErrorContainer,
+          ),
+        ),
+        duration: const Duration(seconds: 2),
+        closeIconColor: theme.colorScheme.onErrorContainer,
+        backgroundColor: theme.colorScheme.errorContainer,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return OutlinedButton.icon(
-      onPressed: () {},
+      onPressed: () => _pickFile(context),
       style: OutlinedButton.styleFrom(
         backgroundColor: theme.colorScheme.secondaryContainer,
       ),
